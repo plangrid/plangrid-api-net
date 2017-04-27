460c26da056c067b3cb50a65a8a6dbd9683ae1f2
BryanWorkLaptop <bryangrom@gmail.com>
2017-04-27 12:58:56 -0700

    Project Properties Updated
    -added address fields present in v1.8.0 of the PlanGrid API

--- PlanGrid.Api/Project.cs
+++ PlanGrid.Api/Project.cs
@@ -10,5 +10,41 @@ namespace PlanGrid.Api
     {
         [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("custom_id")]
+        public string CustomId { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("owner")]
+        public string Owner { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTime? StartDate { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTime? EndDate { get; set; }
+
+        [JsonProperty("street_1")]
+        public string Street1 { get; set; }
+
+        [JsonProperty("street_2")]
+        public string Street2 { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("region")]
+        public string Region { get; set; }
+
+        [JsonProperty("postal_code")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
     }
 }
