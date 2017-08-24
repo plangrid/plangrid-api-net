45c65f8720a20070fa877ded80aa5765f79c0e23
James Palawaga <vsfmag@gmail.com>
2017-08-24 08:59:52 -0700

    New version apis (#13)
    * Fix broken attachment test
    * Add version set api support
    * Add versionset filtering to sheets

--- PlanGrid.Api/IPlanGridApi.cs
+++ PlanGrid.Api/IPlanGridApi.cs
@@ -131,7 +131,7 @@ namespace PlanGrid.Api
         Task<Photo> UpdatePhoto(string projectUid, string photoUid, [Body]PhotoUpdate photo);
 
         [Get("/projects/{projectUid}/sheets")]
-        Task<Page<Sheet>> GetSheets(string projectUid, int skip = Page.Skip, int limit = Page.Limit, DateTime? updated_after = null);
+        Task<Page<Sheet>> GetSheets(string projectUid, int skip = Page.Skip, int limit = Page.Limit, DateTime? updated_after = null, string version_set = null);
 
         [Get("/projects/{projectUid}/snapshots/{snapshotUid}")]
         Task<Snapshot> GetSnapshot(string projectUid, string snapshotUid);
@@ -165,5 +165,8 @@ namespace PlanGrid.Api
 
         [Get("/projects/{projectUid}/snapshots")]
         Task<Page<Snapshot>> GetSnapshots(string projectUid, int skip = Page.Skip, int limit = Page.Limit, DateTime? updated_after = null);
+
+        [Get("/projects/{projectUid}/version_sets")]
+        Task<Page<VersionSet>> GetVersionSets(string projectUid, int skip = Page.Skip, int limit = Page.Limit);
     }
 }
